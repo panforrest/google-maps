f210b0f64d764672de1b4da8ee27b76313e0208b
panforrest <guoqianp@gmail.com>
2017-03-25 21:52:26 -0400

    console logs list of venues

--- src/app.js
+++ src/app.js
@@ -3,8 +3,26 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Map from '../components/Map'
 import Places from '../components/Places'
+import superagent from 'superagent'
 
 class App extends Component {
+    componentDidMount(){
+    	console.log('componentDidMount: ')
+
+    	const url = 'https://api.foursquare.com/v2/venues/search?v=20140806&ll=40.7575285,-73.9884469&client_id=VZZ1EUDOT0JYITGFDKVVMCLYHB3NURAYK3OHB5SK5N453NFD&client_secret=UAA15MIFIWVKZQRH22KPSYVWREIF2EMMH0GQ0ZKIQZC322NZ'
+
+        superagent
+        .get(url)
+        .query(null)
+        .set('Accept', 'text/json')   //.set('Accept', 'application/json') 
+        .end((error, response) => {
+
+        	const venues = response.body.response.venues
+        	console.log(JSON.stringify(venues))  //console.log(JSON.stringify(response.body))
+        })
+    }
+
+
 	render(){
         const location = {
         	lat: 40.7575285,
